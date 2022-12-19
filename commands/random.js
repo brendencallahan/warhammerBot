d3223c9e47940784842f886d818fff7e1c91b344
Brenden Callahan <brendenkcallahan@gmail.com>
2022-12-18 16:05:55 -0800

    Included ability to specify faction, as well as error handling on 'random'

--- commands/random.js
+++ commands/random.js
@@ -4,29 +4,25 @@ module.exports = {
     name: 'random',
     aliases: ['fact', 'funfact'],
     run: async (client, message, args) => {
-
-
         // Get faction name and print random fact if not provided
-        const string = args.join(' ')
-        if (!string) {
-            let factions = []
-            for (i in facts) {
-                factions.push(i)
-            }
-
-            let random_facts_list = facts[factions[Math.floor(Math.random()*factions.length)]]
-            let random_fact = random_facts_list[Math.floor(Math.random()*random_facts_list.length)]
-
-            return message.channel.send(random_fact)
-
+        const string = args.join(' ').toLowerCase()
+        let factions = []
+        for (i in facts) {
+            factions.push(i)
         }
 
+        if (!string) {
+            var facts_list = facts[factions[Math.floor(Math.random()*factions.length)]]
+        }
+        else if (factions.includes(string)){
+            var facts_list = facts[string]
+        }
+        else {
+            return message.channel.send(`${client.emotes.error} | Heretic! I know nothing of this ${string}.`)
+        }
 
-        // Get fact from argument provided faction
-
-
-
-
+        let random_fact = facts_list[Math.floor(Math.random()*facts_list.length)]
+        return message.channel.send(random_fact)
         // Return fact, if found
         /* if (!fact) return message.channel.send(`${client.emotes.error} | Heretic! You know nothing!`)
         return message.channel.send(fact) */
