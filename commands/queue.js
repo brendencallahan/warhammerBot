43cce289cf12a4164623b9e3618e45036f7afb79
Brenden Callahan <brendenkcallahan@gmail.com>
2022-12-18 20:56:34 -0800

    Added error handling for long queue will not display beginning and end

--- commands/queue.js
+++ commands/queue.js
@@ -8,9 +8,20 @@ module.exports = {
     }
     const queue = client.distube.getQueue(message)
     if (!queue) return message.channel.send(`${client.emotes.error} | Heretic! The queue is empty!`)
-    const q = queue.songs
+    var q = queue.songs
       .map((song, i) => `${i === 0 ? 'Playing:' : `${i}.`} ${song.name} - \`${song.formattedDuration}\``)
       .join('\n')
+
+    if (q.length > 1750) {
+      q_first = q.slice(0, 1100)
+      q_last = q.slice(q.length - 650)
+      q = (`
+      ${q_first}
+      .
+      .
+      .
+      ${q_last}`)
+    }
     message.channel.send(`${client.emotes.queue} | **Server Queue**\n${q}`)
   }
 }
