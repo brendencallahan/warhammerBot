04fdbb98c740cf398cff6fc746f6e14c5cd2b8a9
Brenden Callahan <brendenkcallahan@gmail.com>
2022-12-25 11:59:52 -0800

    Added ability to see faction list and have ferrus return a pic when he is mentioned

--- helpers/regex.py
+++ helpers/regex.py
@@ -1,46 +1,65 @@
 import re as reg
+import os
+import click as cli
 
-# Read file into single string
-try:
-    with open('40k facts.txt', 'r') as fin:
-        facts = fin.read()
-except:
-    print("Unable to open file. Ensure it is in this directory and named '40k facts.txt'")
-
-
-
-# Compile the regular expressions
-regFactions = reg.compile(r"(?=\*).*(?=:\n)")
-
-factions = []
-
-res = regFactions.findall(facts)
+def main():
+    # Read file into single string
+    try:
+        with open('../static/40k facts.txt', 'r') as fin:
+            facts = fin.read()
+    except:
+        print("Could not find '40k facts.txt'. Make sure it is in '../static/'")
+        return
 
 
+    if facts:
+        # Make array of facts
+        facts = reg.split("\*", facts)
+        facts.pop(0)
 
-if facts:
-    facts = reg.split("\*", facts)
-    facts.pop(0)
+        for i in range(len(facts)):
 
-    for i in range(len(facts)):
-        #print(test.replace('"', ''))
-        # print(facts[i])
+            # Remove whitespace
+            facts[i] = facts[i].strip()
+
+            # Select all faction names
+            if facts[i].endswith(":"):
+
+                # Delete comma from every entry that is directly before a faction (except the first faction)
+                if i != 0:
+                    facts[i - 1] = f'{facts[i - 1][:-3]}\n],\n\n'
+
+                # Canonize the faction names for JSON format
+                facts[i] = f'"{facts[i].lower().replace(":", "").partition(" ")[0]}":[\n'
+
+            # Format all entries that aren't faction names. Removing whitespace, adding quotation marks and a comma
+            else:
+                facts[i] = f'\t"{facts[i].strip()}",\n\n'
 
 
-        facts[i] = facts[i].strip()
-        if facts[i].endswith(":"):
-            if i != 0:
-                facts[i - 1] = f'{facts[i - 1][:-3]}\n],\n\n'
-            facts[i] = f'"{facts[i].lower().replace(":", "").partition(" ")[0]}":[\n'
+        # Wrap the entire thing in curly braces {
+        facts[0] = "{" + facts[0]
+        # } and remove trailing comma from last entry
+        facts[-1] = facts[-1][:-3] + "\n]}"
 
-
+        # Write to JSON file for Ferrus to read
+        if os.path.isfile('../static/facts.json'):
+            if cli.confirm('facts.JSON exists. Replace?', default=True):
+                print('Replacing facts.json...')
+                with open('../static/facts.json', 'w') as f:
+                    f.writelines(facts)
+                print('Success')
+                return
+            else:
+                print('Exiting job...')
+                print('Exited: "facts.JSON already exists"')
+                return
         else:
-            facts[i] = f'\t"{facts[i].strip()}",\n\n'
+            with open('../static/facts.json', 'w') as f:
+                f.writelines(facts)
+                print('Success')
+                return
 
 
-    facts[0] = "{" + facts[0]
-    facts[-1] = facts[-1][:-3] + "\n]\n}"
-
-
-    with open('facts.json', 'w') as f:
-        f.writelines(facts)
+if __name__ == "__main__":
+    main()
