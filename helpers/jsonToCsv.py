1c61145b08e98dbcfa8e9cf687548265cdbe41d5
Brenden Callahan <brendenkcallahan@gmail.com>
2023-02-13 22:24:28 -0800

    reforma

--- helpers/jsonToCsv.py
+++ helpers/jsonToCsv.py
@@ -5,15 +5,17 @@ with open("../static/facts.json", "r") as jsonfile:
     data = json.load(jsonfile)
     with open("../static/facts.csv", "w", newline="") as csvfile:
         fieldnames = ["alliance", "faction", "subfaction", "fact"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect="excel")
+        writer = csv.DictWriter(
+            csvfile, fieldnames=fieldnames, dialect="excel")
 
         writer.writeheader()
         for faction in data:
             for fact in data[faction]:
-                row = {
-                    "alliance": faction,
-                    "faction": "faction",
-                    "subfaction": "subfaction",
-                    "fact": fact,
-                }
-                writer.writerow(row)
+                row = writer.writerow(
+                    {
+                        "alliance": faction,
+                        "faction": "faction",
+                        "subfaction": "subfaction",
+                        "fact": fact,
+                    }
+                )
