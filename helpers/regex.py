c829058cb77f6c51ba6cc14c486d52501513ea6a
Brenden Callahan <brendenkcallahan@gmail.com>
2023-02-13 16:13:49 -0800

    New Imperium Facts

--- helpers/regex.py
+++ helpers/regex.py
@@ -2,40 +2,39 @@ import re as reg
 import os
 import click as cli
 
+
 def main():
     # Read file into single string
     try:
-        with open('../static/40k facts.txt', 'r') as fin:
+        with open("../static/40k facts.txt", "r") as fin:
             facts = fin.read()
-    except:
-        print("Could not find '40k facts.txt'. Make sure it is in '../static/'")
+    except OSError:
+        print("Could not find '40k facts.txt'. Make sure it's in '../static/'")
         return
-
 
     if facts:
         # Make array of facts
-        facts = reg.split("\*", facts)
+        facts = reg.split("\\*", facts)
         facts.pop(0)
 
         for i in range(len(facts)):
-
             # Remove whitespace
             facts[i] = facts[i].strip()
 
             # Select all faction names
             if facts[i].endswith(":"):
-
-                # Delete comma from every entry that is directly before a faction (except the first faction)
+                # Delete comma from every entry that is before a faction
                 if i != 0:
-                    facts[i - 1] = f'{facts[i - 1][:-3]}\n],\n\n'
+                    facts[i - 1] = f"{facts[i - 1][:-3]}\n],\n\n"
 
                 # Canonize the faction names for JSON format
-                facts[i] = f'"{facts[i].lower().replace(":", "").partition(" ")[0]}":[\n'
+                facts[
+                    i
+                ] = f'"{facts[i].lower().replace(":", "").partition(" ")[0]}":[\n'
 
-            # Format all entries that aren't faction names. Removing whitespace, adding quotation marks and a comma
+            # Format all entries that aren't faction names.
             else:
-                facts[i] = f'\t"{facts[i].strip()}",\n\n'
-
+                facts[i] = f'\t"{facts[i].strip()}",\n'
 
         # Wrap the entire thing in curly braces {
         facts[0] = "{" + facts[0]
@@ -43,24 +42,23 @@ def main():
         facts[-1] = facts[-1][:-3] + "\n]}"
 
         # Check for existing facts.json file
-        if os.path.isfile('../static/facts.json'):
-
+        if os.path.isfile("../static/facts.json"):
             # Replace facts.json, if user confirms, otherwise exit
-            if cli.confirm('facts.JSON exists. Replace?', default=True):
-                print('Replacing facts.json...')
-                with open('../static/facts.json', 'w') as f:
+            if cli.confirm("facts.JSON exists. Replace?", default=True):
+                print("Replacing facts.json...")
+                with open("../static/facts.json", "w") as f:
                     f.writelines(facts)
-                print('Success')
+                print("Success")
                 return
             else:
-                print(f'Exiting job...\n Exited: "facts.JSON already exists"')
+                print('Exiting job...\n Exited: "facts.JSON already exists"')
                 return
 
         # If facts.json doesn't exist create it and write facts to it
         else:
-            with open('../static/facts.json', 'w') as f:
+            with open("../static/facts.json", "w") as f:
                 f.writelines(facts)
-                print(f'Creating facts.json...\n Success')
+                print("Creating facts.json...\n Success")
                 return
 
 
