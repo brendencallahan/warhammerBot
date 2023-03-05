464ab7a8ebffc4611a768e1c0fe03da43c441e2e
Brenden Callahan <brendenkcallahan@gmail.com>
2023-03-04 20:56:02 -0800

    Give GPT Ferrus a memory

--- commands/ask.js
+++ commands/ask.js
@@ -1,12 +1,15 @@
 const axios = require("axios");
-const { OPEN_API } = require("../config.json")
+const { OPEN_API } = require("../config.json");
+let messages = require("../static/memory.json");
+const fs = require("fs");
 
 module.exports = {
   name: "ask",
   description: "Uses *definitely not AI* to answer your question",
   verbose:
     "Typing `!ask` followed by a `query`, will ask Ferrus the question. " +
-    "This is *definitely not AI* but be sure to keep this between us.",
+    "Ferrus will remember everything you say, until you use \`!ask cm\`." +
+    "This is *definitely not AI* but be sure to keep that between us.",
   run: async (client, message, args) => {
     const string = args.join(" ");
     if (!string) {
@@ -14,6 +17,24 @@ module.exports = {
         `${client.emotes.error} | Heretic! Enter a url or query to start playing. See \`!help play\` for more info.`
       );
     }
+
+    if (string === "cm") {
+      //early return if clearing memory
+      messages = [messages.shift()];
+      fs.writeFile("static/memory.json", JSON.stringify(messages), (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+      return message.channel.send(
+        `${client.emotes.success} | Wait... what did you say?`
+      );
+    }
+
+    messages.push({
+      role: "user",
+      content: string,
+    });
 
     axios({
       method: "POST",
@@ -24,25 +45,32 @@ module.exports = {
       url: "https://api.openai.com/v1/chat/completions",
       data: {
         model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "system",
-            content: "You are Ferrus Manus of the Warhammer 40k universe",
-          },
-          { role: "user", content: `${string}` },
-        ],
+        messages: messages,
         temperature: 0,
         max_tokens: 350,
       },
     })
       .then((resp) => {
+        messages.push({
+          role: "assistant",
+          content: resp.data.choices[0].message.content,
+        });
+        fs.writeFile(
+          "static/memory.json",
+          JSON.stringify(messages),
+          (error) => {
+            if (error) {
+              console.log(error);
+            }
+          }
+        );
         return message.channel.send(
           `${client.emotes.success} | ${resp.data.choices[0].message.content}`
         );
       })
       .catch((error) => {
         return message.channel.send(
-          `${client.emotes.success} | ${error.response.data.error.message}`
+          `${client.emotes.error} | ${error.response.data.error.message}`
         );
       });
   },
